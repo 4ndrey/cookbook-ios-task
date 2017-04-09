4b7bf05af5727770a4884015c809e96f962409d5
Andrey Toropchin <4ndrey@gmail.com>
2017-04-09 09:54:36 +0500

    updated networking

--- Cookbook-Test-Project/Domain/Networking/CookbookAPIService.swift
+++ Cookbook-Test-Project/Domain/Networking/CookbookAPIService.swift
@@ -7,11 +7,15 @@
 //
 
 import Foundation
+import Alamofire
 import ReactiveSwift
 import Result
 
 protocol CookbookAPIServicing {
     func getRecipes() -> SignalProducer<[Receipt],RequestError>
+    func getRecipe(id: String) -> SignalProducer<Receipt, RequestError>
+    func rateRecipe(id: String, score: Int) -> SignalProducer<Void, RequestError>
+    func createRecipe(name: String, description: String, info: String, duration: Int, ingredients: [String]) -> SignalProducer<Receipt, RequestError>
 }
 
 /**
@@ -25,7 +29,7 @@ class CookbookAPIService : APIService, CookbookAPIServicing {
         return relativeURL
     }
     
-    internal func getRecipes() -> SignalProducer<[Receipt], RequestError> {
+    func getRecipes() -> SignalProducer<[Receipt], RequestError> {
         return self.request("recipes")
             .mapError { .network($0) }
             .attemptMap { response in
@@ -33,5 +37,31 @@ class CookbookAPIService : APIService, CookbookAPIServicing {
                 do { return Result.success(try [Receipt].decode(items)) }
                 catch { return Result.failure(RequestError.mapping()) }
             }
+    }
+
+    func getRecipe(id: String) -> SignalProducer<Receipt, RequestError> {
+        return self.request("recipes/\(id)")
+            .mapError { .network($0) }
+            .attemptMap { response in
+                guard let item = response else { return Result.failure(RequestError.mapping()) }
+                do { return Result.success(try Receipt.decodeValue(item)) }
+                catch { return Result.failure(RequestError.mapping()) }
+        }
+    }
+
+    func rateRecipe(id: String, score: Int) -> SignalProducer<Void, RequestError> {
+        return self.request("recipes/\(id)/ratings", method: .post, parameters: ["score": score], encoding: JSONEncoding.default)
+            .mapError { .network($0) }
+            .map { _ in }
+    }
+
+    func createRecipe(name: String, description: String, info: String, duration: Int, ingredients: [String]) -> SignalProducer<Receipt, RequestError> {
+        return self.request("recipes", method: .post, parameters: ["name": name, "description": description, "info": info, "duration": duration, "ingredients": ingredients], encoding: JSONEncoding.default)
+            .mapError { .network($0) }
+            .attemptMap { response in
+                guard let item = response else { return Result.failure(RequestError.mapping()) }
+                do { return Result.success(try Receipt.decodeValue(item)) }
+                catch { return Result.failure(RequestError.mapping()) }
+        }
     }
 }
